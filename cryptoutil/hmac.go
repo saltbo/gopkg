dd3705660831c5548db8df1308efcb758c92142f
saltbo <saltbo@foxmail.com>
2020-07-31 16:22:15 +0800

    refactor: remove the signer && add hmac into cryptoutil

--- cryptoutil/hmac.go
+++ cryptoutil/hmac.go
@@ -1,1 +1,13 @@
 package cryptoutil
+
+import (
+	"crypto/hmac"
+	"crypto/sha1"
+	"encoding/hex"
+)
+
+func HmacHex(data, key []byte) string {
+	hmacSha1 := hmac.New(sha1.New, key)
+	hmacSha1.Write(data)
+	return hex.EncodeToString(hmacSha1.Sum(nil))
+}
