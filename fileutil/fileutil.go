9d551886c4332cf6c9f64381a460a7e9627e4840
saltbo <saltbo@foxmail.com>
2020-07-31 18:26:17 +0800

    feat: add MD5Hex and DetectContentType for the fileutil

--- fileutil/fileutil.go
+++ fileutil/fileutil.go
@@ -1,11 +1,19 @@
 package fileutil
 
 import (
+	"crypto/md5"
+	"encoding/hex"
+	"io"
+	"io/ioutil"
+	"mime"
+	"net/http"
 	"os"
+	"path"
 	"path/filepath"
 	"strings"
 )
 
+// PathExist
 func PathExist(path string) bool {
 	if _, err := os.Stat(path); err != nil && os.IsNotExist(err) {
 		return false
@@ -14,6 +22,7 @@ func PathExist(path string) bool {
 	return true
 }
 
+// Visit
 func Visit(dir string, suffix string, visitor func(filename string) error) error {
 	return filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
@@ -26,4 +35,35 @@ func Visit(dir string, suffix string, visitor func(filename string) error) error
 
 		return nil
 	})
+}
+
+// MD5Hex returns the file md5 hash hex
+func MD5Hex(filepath string) string {
+	f, err := os.Open(filepath)
+	if err != nil {
+		return ""
+	}
+	defer f.Close()
+
+	md5hash := md5.New()
+	if _, err := io.Copy(md5hash, f); err != nil {
+		return ""
+	}
+
+	return hex.EncodeToString(md5hash.Sum(nil)[:])
+}
+
+// DetectContentType returns the file content-type
+func DetectContentType(filepath string) string {
+	mimeType := mime.TypeByExtension(path.Ext(filepath))
+	if mimeType != "" {
+		return mimeType
+	}
+
+	fileData, err := ioutil.ReadFile(filepath)
+	if err != nil {
+		return ""
+	}
+
+	return http.DetectContentType(fileData)
 }
