98aabc980d94be1bead2a0558c406d523df55a1d
saltbo <saltbo@foxmail.com>
2020-08-03 20:57:35 +0800

    feat: add timeutil

--- go.mod
+++ go.mod
@@ -4,7 +4,8 @@ go 1.14
 
 require (
 	github.com/gin-gonic/gin v1.6.3
-	github.com/go-resty/resty/v2 v2.3.0 // indirect
+	github.com/go-playground/assert/v2 v2.0.1
+	github.com/go-resty/resty/v2 v2.3.0
 	github.com/stretchr/testify v1.6.1
 	go.uber.org/zap v1.15.0
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0
---
+++ timeutil/timeutil.go
@@ -0,0 +1,46 @@
+package timeutil
+
+import (
+	"strings"
+	"time"
+)
+
+const FormatLayout = "2006-01-02 15:04:05"
+
+type Item struct {
+	Go  string
+	Std string
+}
+
+var formats = []Item{
+	{Std: "YYYY", Go: "2006"},
+	{Std: "YY", Go: "06"},
+	{Std: "MMMM", Go: "January"},
+	{Std: "MMM", Go: "Jan"},
+	{Std: "MM", Go: "01"},
+	{Std: "DD", Go: "02"},
+	{Std: "HH", Go: "15"},
+	{Std: "hh", Go: "03"},
+	{Std: "h", Go: "3"},
+	{Std: "mm", Go: "04"},
+	{Std: "m", Go: "4"},
+	{Std: "ss", Go: "05"},
+	{Std: "s", Go: "5"},
+}
+
+// Format
+// YYYY = 2006，YY = 06
+// MM = 01， MMM = Jan，MMMM = January
+// DD = 02，
+// DDD = Mon，DDDD = Monday
+// HH = 15，hh = 03, h = 3
+// mm = 04, m = 4
+// ss = 05, m = 5
+// Example Format(time.Now(), "YYYY-MM-DD HH:mm:ss")
+func Format(time time.Time, layout string) string {
+	for _, format := range formats {
+		layout = strings.Replace(layout, format.Std, format.Go, 1)
+	}
+
+	return time.Format(layout)
+}
