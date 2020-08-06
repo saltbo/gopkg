1f4fecb05e7caa2b3a15280fb79ccb8e7b7f2865
saltbo <saltbo@foxmail.com>
2020-08-06 20:42:54 +0800

    feat: add reverseproxy into httputil

---
+++ httputil/reverseproxy.go
@@ -0,0 +1,47 @@
+package httputil
+
+import (
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"strings"
+)
+
+type ReverseProxy struct {
+	httputil.ReverseProxy
+}
+
+func singleJoiningSlash(a, b string) string {
+	aslash := strings.HasSuffix(a, "/")
+	bslash := strings.HasPrefix(b, "/")
+	switch {
+	case aslash && bslash:
+		return a + b[1:]
+	case !aslash && !bslash:
+		return a + "/" + b
+	}
+	return a + b
+}
+
+func NewReverseProxy(target *url.URL, header http.Header) *ReverseProxy {
+	targetQuery := target.RawQuery
+	director := func(req *http.Request) {
+		req.URL.Scheme = target.Scheme
+		req.URL.Host = target.Host
+		req.URL.Path = singleJoiningSlash(target.Path, req.URL.Path)
+		if targetQuery == "" || req.URL.RawQuery == "" {
+			req.URL.RawQuery = targetQuery + req.URL.RawQuery
+		} else {
+			req.URL.RawQuery = targetQuery + "&" + req.URL.RawQuery
+		}
+		if _, ok := req.Header["User-Agent"]; !ok {
+			// explicitly disable User-Agent so it's not set to default value
+			req.Header.Set("User-Agent", "")
+		}
+
+		req.Host = target.Host // use the origin host from backend
+		HeaderCopyTo(header, req.Header)
+	}
+
+	return &ReverseProxy{ReverseProxy: httputil.ReverseProxy{Director: director}}
+}
