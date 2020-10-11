4bd9ba845e663cff12077bf9ef020bc481cd532d
saltbo <saltbo@foxmail.com>
2020-10-11 15:26:51 +0800

    refactor: export the httputil.Client

--- httputil/client.go
+++ httputil/client.go
@@ -9,25 +9,25 @@ import (
 	"net/url"
 )
 
-type requestClient struct {
+type Client struct {
 	*Resty
 
 	resourcePath string
 }
 
-func NewClient(endpoint string, resourcePath string) *requestClient {
-	return &requestClient{
+func NewClient(endpoint string, resourcePath string) *Client {
+	return &Client{
 		Resty: NewResty(fmt.Sprintf("http://%s", endpoint)),
 
 		resourcePath: resourcePath,
 	}
 }
 
-func (rc *requestClient) Find(name string, result interface{}) error {
+func (rc *Client) Find(name string, result interface{}) error {
 	return rc.Get(fmt.Sprintf("%s/%s", rc.resourcePath, name), result)
 }
 
-func (rc *requestClient) FindAll(query url.Values, result interface{}) error {
+func (rc *Client) FindAll(query url.Values, result interface{}) error {
 	rs := rc.resourcePath
 	if query != nil {
 		rs += "?" + query.Encode()
@@ -36,14 +36,14 @@ func (rc *requestClient) FindAll(query url.Values, result interface{}) error {
 	return rc.Get(rs, result)
 }
 
-func (rc *requestClient) Create(spec interface{}) error {
+func (rc *Client) Create(spec interface{}) error {
 	return rc.Resty.Create(rc.resourcePath, spec)
 }
 
-func (rc *requestClient) Update(spec interface{}) error {
+func (rc *Client) Update(spec interface{}) error {
 	return rc.Resty.Update(rc.resourcePath, spec)
 }
 
-func (rc *requestClient) Delete(name string) error {
+func (rc *Client) Delete(name string) error {
 	return rc.Resty.Delete(fmt.Sprintf("%s/%s", rc.resourcePath, name))
 }
