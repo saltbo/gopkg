0d57fc71396d6c285e01609bb6ec6ef440d42f3a
saltbo <saltbo@foxmail.com>
2023-07-25 23:31:25 +0800

    feat: add ParamInt64
    Signed-off-by: saltbo <saltbo@foxmail.com>

--- ginutil/input.go
+++ ginutil/input.go
@@ -11,7 +11,17 @@ func ParamInt(c *gin.Context, name string) int {
 	return v
 }
 
+func ParamInt64(c *gin.Context, name string) int64 {
+	v, _ := strconv.ParseInt(c.Param(name), 10, 64)
+	return v
+}
+
 func QueryInt(c *gin.Context, name string) int {
 	v, _ := strconv.Atoi(c.Query(name))
+	return v
+}
+
+func QueryInt64(c *gin.Context, name string) int64 {
+	v, _ := strconv.ParseInt(c.Query(name), 10, 64)
 	return v
 }
